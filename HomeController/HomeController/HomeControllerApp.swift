c426ab0bafeae02ec325b1443781c0722a465aff
Takahashi Oto <ottotak@outlook.com>
2022-06-28 22:51:48 +0900

    init

---
+++ HomeController/HomeController/HomeControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeControllerApp.swift
+//  HomeController
+//
+//  Created by omg on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct HomeControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
